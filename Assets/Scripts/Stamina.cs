f955055a4f046782707c4f5184b0fef2f9c19a45
TeoNaba99 <56259499+TeoNaba99@users.noreply.github.com>
2021-06-04 14:55:12 +0200

    stamina bar

---
+++ Assets/Scripts/Stamina.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Stamina : MonoBehaviour
+{
+    public Slider staminaBar;
+
+    public int maxStamina = 100;
+    private float currentStamina;
+
+
+    private WaitForSeconds regenTick = new WaitForSeconds(0.01f);
+    private Coroutine regen;
+
+    public static Stamina instance;
+
+    public FirstPersonController player;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = maxStamina;
+    }
+
+   
+    void Update()
+    {
+        if(currentStamina > 10)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                UseStamina(0.5f);
+            }
+            player.m_RunSpeed = 10f;
+            player.m_WalkSpeed = 5f;
+        } else if (currentStamina <= 1)
+        {
+            player.m_IsWalking = true;
+            staminaBar.value = currentStamina;
+            player.m_RunSpeed = 3f;
+            player.m_WalkSpeed = 5f; 
+        }
+    }
+
+
+    public void UseStamina(float amount)
+    {
+        if(currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+
+            if(regen != null)
+            {
+                StopCoroutine(regen);
+            }
+
+            regen = StartCoroutine(RegenStamina());
+        }
+    }
+
+    private IEnumerator RegenStamina()
+    {
+        yield return new WaitForSeconds(2);
+
+        while (currentStamina < maxStamina)
+        {
+            currentStamina += maxStamina / 100;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+        }
+    }
+}
