b8b509a3fdac461be58b864533a8f9e78a16278a
MarcoBondesaniNaba <Marco_Bondesani@studenti.naba.it>
2021-07-07 23:07:48 +0200

    shoot script

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    [SerializeField] bool automatic;
+    bool isShooting;
+    bool isReloading;
+    bool readyToShoot;
+
+    int bulletsLeft;
+    public int sizeMagazine;
+    public float _maxdistance;
+
+    public float reloadTime;
+    public float shootingInterval;
+    public float spread;
+
+    [SerializeField] Camera _playerCam;
+
+    RaycastHit _rayhit;
+    LayerMask definedLayer;
+
+
+    private void Awake()
+    {
+        isShooting = false;
+        isReloading = false;
+        reloadTime = 2f;
+        bulletsLeft = sizeMagazine;
+
+
+    }
+
+    private void Update()
+    {
+        if (automatic)
+        {
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else
+        {
+            Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        //Reload
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < sizeMagazine && !isReloading)
+            Reload();
+
+        //Shoot
+        if (isShooting && bulletsLeft > 0 && !isReloading && readyToShoot)
+            Shoot();
+    }
+
+    void Shoot()
+    {
+        readyToShoot = false;
+        float x = Random.RandomRange(-spread, spread);
+        float y = Random.RandomRange(-spread, spread);
+        Vector3 direction = _playerCam.transform.forward + new Vector3(x, y, 0);
+        //raycast
+        if (Physics.Raycast(_playerCam.transform.position, _playerCam.transform.forward, out _rayhit, _maxdistance, definedLayer)) {
+            Debug.Log(_rayhit.collider.name);
+        } 
+
+        bulletsLeft--;
+        Invoke("ReadyToShoot", shootingInterval);
+    }
+
+    void ReadyToShoot() {
+
+    }
+    void Reload() {
+        isReloading = true;
+
+        Invoke("Reloading", reloadTime);
+
+        isReloading = false;
+
+    }
+
+    void Reloading() {
+        bulletsLeft = sizeMagazine;
+    }
+}
