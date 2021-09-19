f0a65d884d7d5a36f2d18d64929926325c562fad
MarcoBondesaniNaba <Marco_Bondesani@studenti.naba.it>
2021-09-19 18:32:49 +0200

    Ricarica vita e visual per danno

--- Assets/Scripts/Shooter/PlayerController.cs
+++ Assets/Scripts/Shooter/PlayerController.cs
@@ -12,10 +12,16 @@ public class PlayerController : MonoBehaviour
     public float invincibleTime;
     public UnityEvent gameOverEvent;
     public GameObject spawners;
+    public Image blood;
+    private float alpha;
+    private float lastReset;
     private void OnEnable()
     {
         singleton = this;
         isHitten = false;
+        alpha = 0f;
+        StartCoroutine("SubstractOne");
+        StartCoroutine("AddOne");
     }
 
     /*void UpdateHealth()
@@ -23,7 +29,16 @@ public class PlayerController : MonoBehaviour
         Color splatterAlpha = redSplatterImage.color;
         splatterAlpha.a = 1 - health / health;
     }*/
-
+    public void Update()
+    {
+        if (alpha > 0f)
+        {
+            changeAlphaBlood(alpha);
+        }
+        else {
+            changeAlphaBlood(0f);
+        }
+    }
     void OnCollisionEnter(Collision coll)
     {
         //print("COLLIDO");
@@ -32,6 +47,7 @@ public class PlayerController : MonoBehaviour
             //print("E prendo Danno");
             isHitten = true;
             health -= 20f;
+            alpha = 0.8f;
             Invoke("isHittenFalse", invincibleTime);
         }
         if (health < 1) {
@@ -46,5 +62,29 @@ public class PlayerController : MonoBehaviour
     }
     public void isHittenFalse() {
         isHitten = false;
+    }
+    private void changeAlphaBlood(float how)
+    {
+        var tempColor = blood.color;
+        tempColor.a = how;
+        blood.color = tempColor;
+    }
+    IEnumerator SubstractOne() {
+        while (true)
+        {
+            alpha -= 0.04f;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+    IEnumerator AddOne()
+    {
+        while (true)
+        {
+            if (alpha < 0 && health<100)
+            {
+                health += 1f;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
